d9b6d5ed6f2fe5a22e181b3a400a15cf35ee4149
Ismayil Taghi-Zada <ismayil.taghi-zada@sabanciuniv.edu>
2025-11-07 01:43:05 +0300

    WIP, getting database to work, still getting connection refused from MySQL server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
-  "fmt"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"sabanci_pharmacy/internal/config"
+	"sabanci_pharmacy/internal/handlers"
+	"sabanci_pharmacy/internal/models"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+type Application struct {
+	Config config.Config
+	DB     *sql.DB
+}
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Printf("Hello and welcome, %s!\n", s)
+	cfg := config.Load()
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	db, err := models.OpenDB(cfg.DB)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Connected to DB")
+
+	app := &Application{
+		Config: cfg,
+		DB:     db,
+	}
+
+	handler := handlers.NewHandler(app.DB, app.Config)
+
+	router := handler.Routes()
+
+	port := fmt.Sprintf(":%s", cfg.Port)
+	fmt.Printf("Listening on port %s\n", port)
+
+	if err := http.ListenAndServe(port, router); err != nil {
+		log.Fatal(err)
+	}
 }
