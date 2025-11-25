bc941d4c571e0776c5c8f2cf964f0c989e710b5c
Ismayil Taghi-Zada <ismayil.taghi-zada@sabanciuniv.edu>
2025-11-25 21:24:48 +0300

    Migrated whatever functionality there was in GoLang to SpringBoot. Still have a lot to do

---
+++ src/main/java/com/cs308_team_3/sabanci_pharmacy/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.cs308_team_3.sabanci_pharmacy.repository;
+
+import com.cs308_team_3.sabanci_pharmacy.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Integer> {
+}
