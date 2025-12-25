700c2e574596846c51c548106ec0008201f977a4
Javidan Gasimli <javidan.gasimli@sabanciuniv.edu>
2025-12-25 20:11:27 +0300

    add findByStatus method to ReviewRepository

--- backend/src/main/java/com/cs308_team_3/sabanci_pharmacy/repository/ReviewRepository.java
+++ backend/src/main/java/com/cs308_team_3/sabanci_pharmacy/repository/ReviewRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review,Integer>{
     List<Review> findByProductId(Integer productId);
+    List<Review> findByStatus(String status);
 }
