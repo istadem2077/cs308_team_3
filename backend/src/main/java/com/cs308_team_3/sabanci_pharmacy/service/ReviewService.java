83650e25f7bdafa6d740012f8c28fab4294c803d
Javidan Gasimli <javidan.gasimli@sabanciuniv.edu>
2025-12-25 20:36:56 +0300

    add updateReviewStatus method

--- backend/src/main/java/com/cs308_team_3/sabanci_pharmacy/service/ReviewService.java
+++ backend/src/main/java/com/cs308_team_3/sabanci_pharmacy/service/ReviewService.java
@@ -72,4 +72,19 @@ public class ReviewService {
     public List<ReviewResponseDto> getPendingReviews() {
         List<Review> reviews = reviewRepository.findByStatus("PENDING");
         return reviews.stream().map(this::mapToResponseDto).collect(Collectors.toList());
+    }
+
+    // 4. Update review status (Approve or Disapprove)
+    public void updateReviewStatus(Integer reviewId, String newStatus) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new RuntimeException("Review not found"));
+
+        // Validate input to ensure only correct statuses are used
+        if (!newStatus.equals("APPROVED") && !newStatus.equals("DISAPPROVED")) {
+            throw new RuntimeException("Invalid status. Use APPROVED or DISAPPROVED.");
+        }
+
+        review.setStatus(newStatus);
+        reviewRepository.save(review);
+    }
 }
