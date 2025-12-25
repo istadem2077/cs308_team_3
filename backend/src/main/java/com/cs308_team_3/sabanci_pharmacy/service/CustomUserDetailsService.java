ba0094e6e19ee92209b2b11d85a2fcbc491c7698
Javidan Gasimli <javidan.gasimli@sabanciuniv.edu>
2025-12-25 18:33:32 +0300

    load user role authorities in CustomUserDetailsService

--- backend/src/main/java/com/cs308_team_3/sabanci_pharmacy/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/cs308_team_3/sabanci_pharmacy/service/CustomUserDetailsService.java
@@ -3,12 +3,13 @@ package com.cs308_team_3.sabanci_pharmacy.service;
 import com.cs308_team_3.sabanci_pharmacy.entity.User;
 import com.cs308_team_3.sabanci_pharmacy.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority; // IMPORT ADDED
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Collections; // IMPORT ADDED
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -21,11 +22,12 @@ public class CustomUserDetailsService implements UserDetailsService {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + email));
 
+
+        // list containing the user role
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
                 user.getPassword(),
-                new ArrayList<>()
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole()))
         );
     }
-
 }
